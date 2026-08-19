758e15a8cb125cbd1c4a0dfacd6da394fe185033
Dan Do <dando985@gmail.com>
2026-08-19 12:46:58 -0500

    updated port comments

--- serverM.h
+++ serverM.h
@@ -3,15 +3,15 @@
 
 #include "common.h"
 
-// UDP port for the main server to listen for backend servers
+// Main server's UDP port
 static const int SERVERM_UDP_PORT = 24407;
 
-// UDP ports for the main server to connect to backend servers
+// Backend server's UDP port
 static const int SERVERM_BACKEND_A_PORT = 21407;
 static const int SERVERM_BACKEND_B_PORT = 22407;
 static const int SERVERM_BACKEND_C_PORT = 23407;
 
-// TCP port for clients to connect to the main server
+// Main server's TCP ports to listen for client and monitor
 static const int SERVERM_CLIENT_TCP_PORT = 25407;
 static const int SERVERM_MONITOR_TCP_PORT = 26407;
 
