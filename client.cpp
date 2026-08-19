8a76479ebfcf45d079e40043b8336a807dc18b61
Dan Do <dando985@gmail.com>
2026-08-19 12:15:13 -0500

    added comments to client.cpp for better readability

--- client.cpp
+++ client.cpp
@@ -1,9 +1,11 @@
 #include "client.h"
 
-// Helper to send a request to the main server via TCP and receive the response
+// Helper to send a request (E.g., CHECK|username or TX|sender|receiver|amount) to the main server via TCP and receive the response
 static std::string request_via_tcp(const std::string &request) {
+    // Create a TCP client socket with fixed loopback IP address (127.0.0.1) and dynamic port number
     int sockfd = open_tcp_client_socket();
 
+    // Construct a socket address structure of the main server
     sockaddr_in servaddr{};
     servaddr.sin_family = AF_INET;
     servaddr.sin_addr.s_addr = inet_addr(TXCHAIN_HOST);
@@ -16,31 +18,43 @@ static std::string request_via_tcp(const std::string &request) {
         return "";
     }
 
-    // Send the request, shut down the writing side of the socket, receive the response, and close the socket
+    // Send the request
     send_all_tcp(sockfd, request);
+
+    // Disable communication on client socket to signal to main server that it is finished sending the request.
     shutdown(sockfd, SHUT_WR);
+
+    // Receive the response from the server
     std::string resp = recv_all_tcp(sockfd);
+
+    // Close the socket after receiving the response
     close(sockfd);
+
     return resp;
 }
 
-// Prints responses to the console based on the type of request made by the user
+// Takes either a CHECK request (for balance enquiry) or a TX request (for transferring txcoins) and sends it to the main server via TCP. Returns the response from the main server.
 int main(int argc, char *argv[]) {
     std::cout << "The client is up and running." << std::endl;
 
-    // Handle CHECK requests for balance enquiry and TX requests for transferring txcoins
     if (argc == 2) {
+        // Construct the request string
         std::string username = argv[1];
         std::string req = std::string("CHECK|") + username;
         std::cout << "\"" << username << "\" sent a balance enquiry request to the main server." << std::endl;
+
+        // Send request string to main server
         std::string resp = request_via_tcp(req);
         std::cout << resp;
     } else if (argc == 4) {
+        // Construct the transfer string
         std::string sender = argv[1];
         std::string receiver = argv[2];
         std::string amount = argv[3];
         std::string req = std::string("TX|") + sender + "|" + receiver + "|" + amount;
         std::cout << "\"" << sender << "\" has requested to transfer " << amount << " txcoins to \"" << receiver << "\"." << std::endl;
+
+        // Send the request to main server
         std::string resp = request_via_tcp(req);
         std::cout << resp;
     }
